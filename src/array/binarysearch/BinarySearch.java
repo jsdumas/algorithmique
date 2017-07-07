28fd28987d93de373510d465db67e71547396243
Jean-Sébastien Dumas <jsdumas@free.fr>
2017-07-07 18:09:07 +0200

    BinarySearchTest

--- src/array/binarysearch/BinarySearch.java
+++ src/array/binarysearch/BinarySearch.java
@@ -1,24 +1,29 @@
 package array.binarysearch;
 
 // Recherche dichotomique dans un tableau trié
-public class BinarySearch {
+public class BinarySearch<E> {
 
-	private final int[] tableau;
+	private int[] tableauInt;
+	private E[] tableauE;
 
-	public BinarySearch(int[] tableau) {
-		this.tableau = tableau;
+	public BinarySearch(int[] tableauInt) {
+		this.tableauInt = tableauInt;
 	}
 
-	static boolean binarySearch(int[] tableau, int valeur) {
-		int lo = 0, hi = tableau.length; // on cherche dans [lo..hi[
-		while (lo < hi) {
-			int m = lo + (hi - lo) / 2;
-			if (tableau[m] == valeur)
+	public BinarySearch(E[] tableauE) {
+		this.tableauE = tableauE;
+	}
+
+	public boolean isValueFinded(int valeur) {
+		int lo = 0, tableauLength = tableauInt.length; // on cherche dans [lo..hi[
+		while (lo < tableauLength) {
+			int m = lo + (tableauLength - lo) / 2;
+			if (tableauInt[m] == valeur)
 				return true;
-			if (tableau[m] < valeur)
+			if (tableauInt[m] < valeur)
 				lo = m + 1;
 			else
-				hi = m - 1;
+				tableauLength = m - 1;
 		}
 		return false;
 	}
@@ -26,16 +31,16 @@ public class BinarySearch {
 	// une version générique
 	// renvoie la position où l'élément se trouve / devrait être inséré
 
-	static <E extends Comparable<E>> int binarySearch(E[] a, E v) {
-		return binarySearch(a, v, 0, a.length);
+	public <E extends Comparable<E>> int findNumberOccurence(E v) {
+		return findNumberOccurence(v, 0, tableauE.length);
 	}
 
 	// le code ci-dessous cherche dans l'intervalle ouvert [lo..hi[
 
-	static <E extends Comparable<E>> int binarySearch(E[] a, E v, int lo, int hi) {
+	private <E extends Comparable<E>> int findNumberOccurence(E v, int lo, int hi) {
 		while (lo < hi) {
 			int mid = lo + (hi - lo) / 2;
-			int c = a[mid].compareTo(v);
+			int c = ((Comparable<E>) tableauE[mid]).compareTo(v);
 			if (c == 0)
 				return mid;
 			if (c < 0)
