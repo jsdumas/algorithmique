19b595917ff4a32a7ac82c2a88feff859fa1a15b
Jean-Sébastien Dumas <jsdumas@free.fr>
2017-07-10 11:54:21 +0200

    Clean code et communication

--- src/main/java/array/binarysearch/BinarySearchInt.java
+++ src/main/java/array/binarysearch/BinarySearchInt.java
@@ -11,15 +11,18 @@ public class BinarySearchInt {
 
 	public boolean isValueFinded(int valeur) {
 		int indiceDebutRecherche = 0;
-		int tableauLength = tableauInt.length; // on cherche dans [lo..hi[
+		int tableauLength = tableauInt.length; // on cherche dans [indiceDebutRecherche..tableauLength[
 		while (indiceDebutRecherche < tableauLength) {
-			int milieu = indiceDebutRecherche + (tableauLength - indiceDebutRecherche) / 2;
-			if (tableauInt[milieu] == valeur)
+			int indiceMilieuDuTableau = indiceDebutRecherche + (tableauLength - indiceDebutRecherche) / 2;
+			if (tableauInt[indiceMilieuDuTableau] == valeur)
 				return true;
-			if (tableauInt[milieu] < valeur)
-				indiceDebutRecherche = milieu + 1;
+			// Si la valeur est plus petite que la valeur du tableau à l'indice du milieu
+			// alors on continue à chercher dans la partie supérieure du tableau
+			if (tableauInt[indiceMilieuDuTableau] < valeur)
+				indiceDebutRecherche = indiceMilieuDuTableau + 1;
+			// sinon on cherche dans la partie du tableau inférieur à l'indice du milieu
 			else
-				tableauLength = milieu - 1;
+				tableauLength = indiceMilieuDuTableau - 1;
 		}
 		return false;
 	}
--- src/test/java/array/binarysearch/BinarySearchETest.java
+++ src/test/java/array/binarysearch/BinarySearchETest.java
@@ -11,19 +11,19 @@ public class BinarySearchETest {
 	private static final Integer[] TABLEAU = { 1, 2, 3, 5, 10 };
 
 	@Test
-	public void shouldFindANumberInArray() {
+	public void shouldFindIndiceWhereValueIs() {
 		BinarySearchE binarySearchETableau = new BinarySearchE(TABLEAU);
-		assertThat(binarySearchETableau.findNumberOccurence(3), is(2));
-		assertThat(binarySearchETableau.findNumberOccurence(4), is(3));
-		assertThat(binarySearchETableau.findNumberOccurence(10), is(4));
-		assertThat(binarySearchETableau.findNumberOccurence(11), is(5));
+		assertThat(binarySearchETableau.getValueSearchedIndice(3), is(2));
+		assertThat(binarySearchETableau.getValueSearchedIndice(4), is(3));
+		assertThat(binarySearchETableau.getValueSearchedIndice(10), is(4));
+		assertThat(binarySearchETableau.getValueSearchedIndice(11), is(5));
 	}
 
 	@Test
-	public void shouldNotFindANumberInArray() {
-		BinarySearchE binarySearchTableau2 = new BinarySearchE(TABLEAU);
-		assertThat(binarySearchTableau2.findNumberOccurence(0), is(0));
-		assertThat(binarySearchTableau2.findNumberOccurence(1), is(0));
+	public void shouldFindIndiceWhereValueShouldInsert() {
+		BinarySearchE binarySearchETableau = new BinarySearchE(TABLEAU);
+		assertThat(binarySearchETableau.getValueSearchedIndice(0), is(0));
+		assertThat(binarySearchETableau.getValueSearchedIndice(1), is(0));
 	}
 
 }
