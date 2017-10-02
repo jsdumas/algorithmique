4bb98fc7fbeea077cf3abe5911bb287e5a9eddc3
Jean-Sébastien <jsdumas@free.fr>
2017-10-02 22:36:30 +0200

    Algorithm Hanoi Tower start TU

--- src/main/java/algorithms/hb/problems/recursiveproblems/towersofhanoi/TowerOfHanoi.java
+++ src/main/java/algorithms/hb/problems/recursiveproblems/towersofhanoi/TowerOfHanoi.java
@@ -2,15 +2,14 @@ package algorithms.hb.problems.recursiveproblems.towersofhanoi;
 
 public class TowerOfHanoi {
 
-	public void solveHanoiProblem(int n, char rodFrom, char middleRod, char rodTo){
-		
-		if( n == 1 ){
+	// Exponential algorithm time
+	public void solveHanoiProblem(int plateNumber, char rodFrom, char middleRod, char rodTo){
+		if( plateNumber == 1 ){
 			System.out.println("Plate 1 from " + rodFrom + " to " + rodTo);
 			return;
 		}
-		
-		solveHanoiProblem(n-1, rodFrom, rodTo, middleRod);
-		System.out.println("Plate " + n + " from " + rodFrom + " to " + rodTo);
-		solveHanoiProblem(n-1, middleRod, rodFrom, rodTo);
+		solveHanoiProblem(plateNumber-1, rodFrom, rodTo, middleRod);
+		System.out.println("Plate " + plateNumber + " from " + rodFrom + " to " + rodTo);
+		solveHanoiProblem(plateNumber-1, middleRod, rodFrom, rodTo);
 	}
 }
---
+++ src/test/java/test/algorithms/hb/problems/recursiveproblems/towersofhanoi/TowerOfHanoiTest.java
@@ -0,0 +1,24 @@
+package test.algorithms.hb.problems.recursiveproblems.towersofhanoi;
+
+import algorithms.hb.problems.recursiveproblems.towersofhanoi.TowerOfHanoi;
+
+public class TowerOfHanoiTest {
+	
+	private static final TowerOfHanoi TOWER_OF_HANOI = new TowerOfHanoi();
+	
+//	@Test
+//	public void (){
+//		
+//		TOWER_OF_HANOI.solveHanoiProblem(3, 'A', 'B', 'C');
+//		
+//	}
+
+	
+//	Plate 1 from A to C
+//	Plate 2 from A to B
+//	Plate 1 from C to B
+//	Plate 3 from A to C
+//	Plate 1 from B to A
+//	Plate 2 from B to C
+//	Plate 1 from A to C
+}
