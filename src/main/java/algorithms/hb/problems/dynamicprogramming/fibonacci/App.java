bc92ff90f285bf49a43d175ae4f30e78a199a04d
Jean-Sébastien <jsdumas@free.fr>
2017-10-02 22:06:18 +0200

    Algorithm problems refactor

---
+++ src/main/java/algorithms/hb/problems/dynamicprogramming/fibonacci/App.java
@@ -0,0 +1,11 @@
+package algorithms.hb.problems.dynamicprogramming.fibonacci;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		FibonacciAlgorithm fibonacciAlgorithm = new FibonacciAlgorithm();
+		System.out.println(fibonacciAlgorithm.fibonacciMemoize(3));
+
+	}
+}
