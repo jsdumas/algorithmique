13f224e1f5d9344e795935506d4a84138ce7f8db
Jean-Sébastien Dumas <jsdumas@free.fr>
2017-10-04 13:44:29 +0200

    Algorithm quick select TU

--- src/main/java/algorithms/hb/problems/quickselect/Quickselect.java
+++ src/main/java/algorithms/hb/problems/quickselect/Quickselect.java
@@ -4,44 +4,37 @@ import java.util.Random;
 
 public class Quickselect {
 
-	private int[] nums;
+	private final Integer[] nums;
 
-	public Quickselect(int[] nums) {
+	public Quickselect(Integer[] nums) {
 		this.nums = nums;
 	}
 
-	public int select(int k) {
-		return select(0, nums.length - 1, k - 1);
+	// Selection of the n largest number in an array [nums]
+	public int select(int theNLargestNumber) {
+		return select(0, nums.length - 1, theNLargestNumber - 1);
 	}
 
 	private int partition(int indexFirst, int indexLast) {
-
-		int pivot = new Random().nextInt(indexLast - indexFirst + 1) + indexFirst;		
-		
+		int pivot = new Random().nextInt(indexLast - indexFirst + 1) + indexFirst;
 		swap(indexLast, pivot);
-
 		for (int i = indexFirst; i < indexLast; i++) {
 			if (nums[i] > nums[indexLast]) {
 				swap(i, indexFirst);
 				indexFirst++;
 			}
 		}
-
 		swap(indexFirst, indexLast);
-
 		return indexFirst;
 	}
 
 	private int select(int indexFirst, int indexLast, int k) {
-
 		int pivot = partition(indexFirst, indexLast);
-
 		if (pivot > k) {
 			return select(indexFirst, pivot - 1, k);
 		} else if (pivot < k) {
 			return select(pivot + 1, indexLast, k);
 		}
-		
 		return nums[k];
 	}
 
@@ -49,5 +42,9 @@ public class Quickselect {
 		int tmp = nums[i];
 		nums[i] = nums[j];
 		nums[j] = tmp;
+	}
+
+	public Integer[] getNums() {
+		return nums;
 	}
 }
