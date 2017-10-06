cde7b640c04510c35daf9fe76c20d8e4d030c859
Jean-Sébastien Dumas <jsdumas@free.fr>
2017-10-06 17:07:31 +0200

    Design pattern adapter duck UT

--- src/test/java/test/designpatterns/hf/structural/adapter/ducks/pattern/DuckAdapterTest.java
+++ src/test/java/test/designpatterns/hf/structural/adapter/ducks/pattern/DuckAdapterTest.java
@@ -13,10 +13,16 @@ public class DuckAdapterTest {
 	private static final MallardDuck DUCK = new MallardDuck();
 	private static final Turkey TURKEY_ADAPTED_LIKE_A_DUCK = new DuckAdapter(DUCK);
 	private static final String DUCK_QUACK = "Quack like a Mallard Duck.";
+	private static final String FLY = "I'm flying proudly like a Mallard Duck.";
 
 	@Test
 	public void aTurkeyCanQuackLikeADuck() {
 		MatcherAssert.assertThat(TURKEY_ADAPTED_LIKE_A_DUCK.gobble(), Matchers.equalTo(DUCK_QUACK));
+	}
+
+	@Test
+	public void aTurkeyCanFlyLikeADuck() {
+		MatcherAssert.assertThat(TURKEY_ADAPTED_LIKE_A_DUCK.fly(), Matchers.equalTo(FLY));
 	}
 
 }
