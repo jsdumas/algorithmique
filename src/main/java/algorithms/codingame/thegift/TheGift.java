1affabbb47ce15809cc509fe2dcf1e0f7f2ff316
jsdumas <jsdumas@ubuntu>
2017-10-23 10:27:32 -0700

    The Gift UT

---
+++ src/main/java/algorithms/codingame/thegift/TheGift.java
@@ -0,0 +1,17 @@
+package algorithms.codingame.thegift;
+
+import java.util.List;
+
+public class TheGift {
+
+	private int giftValue;
+
+	public TheGift(int giftValue) {
+		this.giftValue = giftValue;
+	}
+
+	public List<Integer> calculateParticipation() {
+		return null;
+	}
+
+}
