7e0ce6712b323c342e16ea095919f34f7c1cce11
Jean-Sébastien Dumas <jsdumas@free.fr>
2017-10-30 10:39:18 -0700

    Bender PathFinderToSuicideCase implementation

--- src/main/java/algorithms/codingame/bender/PathFromBier.java
+++ src/main/java/algorithms/codingame/bender/PathFromBier.java
@@ -1,14 +1,10 @@
 package algorithms.codingame.bender;
 
-public class PathFromBier implements PathFinder {
+public class PathFromBier extends PathFinderToSuicideCase implements PathFinder {
 
 	@Override
 	public Case getNextCase(CaseArea area) {
-		Case suicideCase = area.getSuicideCase();
-		if(suicideCase!=null) {
-			return suicideCase;
-		}
-		return area.getSouth();
+		return findSuicideCase(area);
 	}
 
 }
--- src/main/java/algorithms/codingame/bender/PathFromEmptyCase.java
+++ src/main/java/algorithms/codingame/bender/PathFromEmptyCase.java
@@ -1,14 +1,10 @@
 package algorithms.codingame.bender;
 
-public class PathFromEmptyCase implements PathFinder {
+public class PathFromEmptyCase extends PathFinderToSuicideCase implements PathFinder {
 
 	@Override
 	public Case getNextCase(CaseArea area) {
-		Case suicideCase = area.getSuicideCase();
-		if(suicideCase!=null) {
-			return suicideCase;
-		}
-		return area.getSouth();
+		return findSuicideCase(area);
 	}
 
 }
