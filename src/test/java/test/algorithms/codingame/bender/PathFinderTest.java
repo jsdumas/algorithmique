de904e91edc686a4c54d3a8a41a618b1a1116f04
Jean-Sébastien Dumas <jsdumas@free.fr>
2017-11-09 09:11:50 +0100

    Bender UT method renaming

--- src/test/java/test/algorithms/codingame/bender/PathFinderTest.java
+++ src/test/java/test/algorithms/codingame/bender/PathFinderTest.java
@@ -23,7 +23,7 @@ import org.junit.Test;
 import algorithms.codingame.bender.MemorisePath;
 
 public class PathFinderTest {
-	
+
 	private static final PathFinderFactory PATH_FINDER_FACTORY = new PathFinderFactory();
 	private static final MemorisePath PATH_TO_SOUTH_FROM_START = PATH_FINDER_FACTORY.getPath(TO_SOUTH_FROM_START);
 	private static final MemorisePath PATH_IS_LOOP_BECAUSE_OF_CHARP = PATH_FINDER_FACTORY.getPath(LOOP_BECAUSE_OF_CHARP);
@@ -35,56 +35,56 @@ public class PathFinderTest {
 	private static final MemorisePath PATH_TO_WEST_FROM_INVERSOR = PATH_FINDER_FACTORY.getPath(TO_WEST_FROM_INVERSOR);
 	private static final MemorisePath PATH_TO_SOUTH_FROM_BIER = PATH_FINDER_FACTORY.getPath(TO_SOUTH_FROM_BIER);
 	private static final MemorisePath PATH_TO_SOUTH_FROM_T = PATH_FINDER_FACTORY.getPath(TO_SOUTH_FROM_T);
-	
-	
+
 	@Test
-	public void shouldGetSouthFromStart() {
+	public void whenBenderIsOnStartCaseThenHeShouldWalkToSouth() {
 		assertThat(PATH_TO_SOUTH_FROM_START.printDirection(), equalTo(SOUTH.toString()));
 	}
 
 	@Test
-	public void shouldGetLoopFromStartBecauseOfCharp() {
+	public void whenBenderCanNotWalkToNextCaseBecauseOfCharpCasesAreaThenBenderWalkOnLoop() {
 		assertThat(PATH_IS_LOOP_BECAUSE_OF_CHARP.printDirection(), equalTo(LOOP.toString()));
 	}
-	
+
 	@Test
-	public void shouldGetLoopFromStartBecauseOfX() {
+	public void whenBenderCanNotWalkToNextCaseBecauseOfXCasesAreaThenBenderWalkOnLoop() {
 		assertThat(PATH_IS_LOOP_BECAUSE_OF_X.printDirection(), equalTo(LOOP.toString()));
 	}
-	
+
 	@Test
-	public void shouldGetSouthFromSouthModifier() {
-		assertThat(PATH_TO_SOUTH_FROM_S_MODIFIER.printDirection(), equalTo(SOUTH.toString()+"\n"+SOUTH.toString()));
+	public void whenBenderWalkOnSouthModifierCaseThenBenderWalkToSouth() {
+		assertThat(PATH_TO_SOUTH_FROM_S_MODIFIER.printDirection(), equalTo(SOUTH.toString() + "\n" + SOUTH.toString()));
 	}
-	
+
 	@Test
-	public void shouldGetSouthFromEastModifier() {
-		assertThat(PATH_TO_EAST_FROM_E_MODIFIER.printDirection(), equalTo(SOUTH.toString()+"\n"+EAST.toString()));
+	public void whenBenderWalkOnEastModifierCaseThenBenderWalkToEast() {
+		assertThat(PATH_TO_EAST_FROM_E_MODIFIER.printDirection(), equalTo(SOUTH.toString() + "\n" + EAST.toString()));
 	}
-	
+
 	@Test
-	public void shouldGetNorthFromNorthModifier() {
-		assertThat(PATH_TO_NORTH_FROM_N_MODIFIER.printDirection(), equalTo(SOUTH.toString()+"\n"+EAST.toString()+"\n"+NORTH.toString()));
+	public void whenBenderWalkOnNorthModifierCaseThenBenderWalkToNorth() {
+		assertThat(PATH_TO_NORTH_FROM_N_MODIFIER.printDirection(), equalTo(SOUTH.toString() + "\n" + EAST.toString() + "\n" + NORTH
+				.toString()));
 	}
-	
+
 	@Test
-	public void shouldGetWestFromWestModifier() {
-		assertThat(PATH_TO_WEST_FROM_W_MODIFIER.printDirection(), equalTo(SOUTH.toString()+"\n"+WEST.toString()));
+	public void whenBenderWalkOnWestModifierCaseThenBenderWalkToWest() {
+		assertThat(PATH_TO_WEST_FROM_W_MODIFIER.printDirection(), equalTo(SOUTH.toString() + "\n" + WEST.toString()));
 	}
-	
+
 	@Test
-	public void shouldGetWestFromInversor() {
-		assertThat(PATH_TO_WEST_FROM_INVERSOR.printDirection(), equalTo(SOUTH.toString()+"\n"+WEST.toString()));
+	public void whenBenderWalkOnInversorCaseThenBenderWalkToWest() {
+		assertThat(PATH_TO_WEST_FROM_INVERSOR.printDirection(), equalTo(SOUTH.toString() + "\n" + WEST.toString()));
 	}
-	
+
 	@Test
-	public void shouldGetSouthFromBier() {
-		assertThat(PATH_TO_SOUTH_FROM_BIER.printDirection(), equalTo(SOUTH.toString()+"\n"+SOUTH.toString()+"\n"+SOUTH.toString()));
+	public void whenBenderWalkOnBierCaseThenBenderWalkToSouth() {
+		assertThat(PATH_TO_SOUTH_FROM_BIER.printDirection(), equalTo(SOUTH.toString() + "\n" + SOUTH.toString() + "\n" + SOUTH.toString()));
 	}
-	
+
 	@Test
-	public void shouldGetSouthFromTeleporter() {
-		assertThat(PATH_TO_SOUTH_FROM_T.printDirection(), equalTo(SOUTH.toString()+"\n"+SOUTH.toString()));
+	public void whenBenderWalkOnTeleporterCaseThenBenderWalkToSouth() {
+		assertThat(PATH_TO_SOUTH_FROM_T.printDirection(), equalTo(SOUTH.toString() + "\n" + SOUTH.toString()));
 	}
 
 }
